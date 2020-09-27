c9eb01d2688e4fe022679f0af704a863a4b198d8
luis felipe <luisfelipedasilvamachadoo@gmail.com>
2020-09-27 11:16:26 -0300

    jogo da velha

---
+++ z/src/Apsjava/Computador.java
@@ -0,0 +1,18 @@
+package Apsjava;
+public class Computador extends Jogador{
+    
+    public Computador(int jogador){
+        super(jogador);
+        System.out.println("Jogador 'Computador' .");
+    }
+    
+    @Override
+    public void jogar(Tabuleiro tabuleiro){
+        
+    }
+    
+    @Override
+    public void Tentativa(Tabuleiro tabuleiro){
+        
+    }
+}
---
+++ z/src/Apsjava/JogoDaVelha.java
@@ -0,0 +1,11 @@
+package Apsjava;
+public class JogoDaVelha {
+
+    public static void main(String[] args) {
+        Jogo jogo = new Jogo();
+        
+        
+    }
+    
+    
+}
